14750880a663fbe8d94f30915ca1dc9ccd2e3ccc
manalab <58519521+manalab@users.noreply.github.com>
2022-04-17 17:46:24 +0300

    Update homePage.dart

--- center_side1/lib/homePage.dart
+++ center_side1/lib/homePage.dart
@@ -1,40 +1,56 @@
 import 'package:center_side/pages/drawer.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/src/material/app.dart';
-//import 'package:carousel_slider/carousel_slider.dart';
 import 'pages/drawer.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 class HomePage extends StatefulWidget {
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return HomeState();
   }
 }
 
-class HomeState extends State<HomePage>{
+class HomeState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Directionality(textDirection: TextDirection.rtl,
-      child:Scaffold(
-      appBar: AppBar(
-        title: Text("Home Page"),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-        elevation: 6,
-        actions: [
-          IconButton(onPressed:(){}, icon: Icon(Icons.home)),
-        ],
-      ),
-      drawer: MyDrawer(),
-      body: ListView(
-        children: [
-          Container(
-            height: 200,
-            width: double.infinity
-            ),
-          ])
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Home Page"),
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+            elevation: 6,
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+            ],
+          ),
+          drawer: MyDrawer(),
+          body: Container(
+              color: Colors.white,
+              child: ListView(children: [
+                Container(
+                    height: 400,
+                    width: double.infinity,
+                    child: Container(
+                      height: 400,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                              'assets/assets/logo.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+
+                ),
+
+              ])
+          )
       ),
     );
   }
-
 }
