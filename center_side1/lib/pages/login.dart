048a181288f7879b4cd1f65db8963cc96b4066d3
manalab <58519521+manalab@users.noreply.github.com>
2022-04-18 15:26:25 +0300

    Update login.dart

--- center_side1/lib/pages/login.dart
+++ center_side1/lib/pages/login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:center_side/dbHelper/user_management.dart';
+import 'package:center_side/dbHelper/user_management.dart';
+import 'package:center_side/example2.dart';
 
 class LogIn extends StatefulWidget {
   const LogIn({Key? key}) : super(key: key);
@@ -8,6 +11,20 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  final TextEditingController _userName= TextEditingController();
+  final TextEditingController _password= TextEditingController();
+
+  void chekUser()async{
+    var user=await searchUser(_userName.text);
+    if(user.password==_password.text){
+      print("ok");
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>(const examplePage2())),);
+    }
+    else print("no match");
+
+    if(user==null) print("user not found");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Directionality(textDirection: TextDirection.rtl, child: Scaffold(
@@ -15,6 +32,13 @@ class _LogInState extends State<LogIn> {
         title: Text('כניסה למערכת'),
         centerTitle: true,
       ),
+      body: ListView(children: [
+        Form(child: Column(children: [
+          TextFormField(
+            decoration: InputDecoration(hintText: null),
+          )
+        ],))
+      ],)
     ));
 
   }
