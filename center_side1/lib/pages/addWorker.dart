3fc7ee927a8740c6ff1b603fe7d40c72af73bdff
manalab <58519521+manalab@users.noreply.github.com>
2022-04-23 08:29:30 +0300

    Update addWorker.dart

--- center_side1/lib/pages/addWorker.dart
+++ center_side1/lib/pages/addWorker.dart
@@ -1,5 +1,11 @@
 import 'package:center_side/compount/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:center_side/compount/drawer.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../dbHelper/worker_managment.dart';
+import '../dbHelper/worker_model.dart';
+import '../example2.dart';
 
 class AddWorker extends StatefulWidget {
   const AddWorker({Key? key}) : super(key: key);
@@ -9,6 +15,149 @@ class AddWorker extends StatefulWidget {
 }
 
 class _AddWorkerState extends State<AddWorker> {
+  final TextEditingController _fullName= TextEditingController();
+  final TextEditingController _userName= TextEditingController();
+  final TextEditingController _password= TextEditingController();
+
+  void addWorker()async{
+    var user=await searchUser(_userName.text);
+    if(user.password==_password.text){
+      print("ok");
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>(const examplePage2())),);
+    }
+    else print("no match");
+
+    if(user==null) print("user not found");
+  }
+
+  Widget NextButton()=>Container(
+    height: 100.0,
+    width: 200.0,
+    child:
+    FloatingActionButton(
+      //child: Icon(Icons.ac_unit),
+      child: Text("הוספה",style: TextStyle(fontSize: 20,color: Colors.black),),
+
+      backgroundColor: Colors.blue,
+      onPressed: () {
+        print("Next");
+        addWorker();
+
+      },
+    ),
+  );
+
+  Widget fullNameTextField()=>Container(
+    height:100,
+    width:200,
+    child: TextField(
+      decoration: InputDecoration(
+          hintText: "fullName",
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color:Colors.black, width: 5.0),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          focusedBorder:OutlineInputBorder(
+            borderSide: const BorderSide(color:Colors.black, width: 2.0),
+            borderRadius: BorderRadius.circular(20.0) ,
+
+          ),
+          fillColor: Colors.white,
+          filled: true,
+          prefix: const Padding(
+            padding: EdgeInsets.all(4),
+          ) ),
+      maxLines: 1,
+      maxLength: 20,
+      controller: _fullName,
+    ),
+  );
+
+  Widget userNameTextField()=>Container(
+    height:100,
+    width:200,
+    child: TextField(
+      decoration: InputDecoration(
+          hintText: "userName",
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color:Colors.black, width: 5.0),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          focusedBorder:OutlineInputBorder(
+            borderSide: const BorderSide(color:Colors.black, width: 2.0),
+            borderRadius: BorderRadius.circular(20.0) ,
+
+          ),
+          fillColor: Colors.white,
+          filled: true,
+          prefix: const Padding(
+            padding: EdgeInsets.all(4),
+          ) ),
+      maxLines: 1,
+      maxLength: 20,
+      controller: _userName,
+    ),
+  );
+
+
+  Widget passwordTextField()=>Container(
+    height:100,
+    width:200,
+    child: TextField(
+      decoration: InputDecoration(
+          hintText: "password",
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color:Colors.black, width: 5.0),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          focusedBorder:OutlineInputBorder(
+            borderSide: const BorderSide(color:Colors.black, width: 2.0),
+            borderRadius: BorderRadius.circular(20.0) ,
+
+          ),
+          fillColor: Colors.white,
+          filled: true,
+          prefix: const Padding(
+            padding: EdgeInsets.all(4),
+          ) ),
+      maxLines: 1,
+      maxLength: 20,
+      controller: _password,
+    ),
+  );
+
+
+  Widget inputContainer()=>Container(
+    height:400,
+    width:400,
+    color: Colors.black,
+    padding: EdgeInsets.all(8),
+    child: Stack(children: [
+      Align(alignment: const Alignment(0, -1),child: fullNameTextField(),),
+      Align(alignment: const Alignment(0, -2),child: userNameTextField(),),
+      Align(alignment: const Alignment(0, 0),child: passwordTextField(),),
+
+    ],),
+  );
+  Widget mainStack()=>Stack(children: [
+    ListView(children: [
+      Container(
+          height: 100,
+          width: double.infinity,
+
+
+      ),
+      Container(
+        child:Align(alignment: const Alignment(0, 0),child: inputContainer(),),
+      ),
+      // Container(
+      //   child:Align(alignment: const Alignment(0, 0.8),child: NextButton(),),
+      // )
+    ],)
+
+
+  ],);
+
   @override
   Widget build(BuildContext context) {
     return Directionality(
@@ -23,17 +172,8 @@ class _AddWorkerState extends State<AddWorker> {
           ),
           backgroundColor: Colors.white,
           drawer: MyDrawer(),
-          body:SafeArea(child:ListView(children: [
-        Text("",style: TextStyle(fontSize: 22),),
-
-        Container(
-          color: Colors.white,
-          height: 100,
-          width: 100,
-          child:Text("hallo")
-        ),
-          ],),),),
+          body:mainStack(),
+      ),
     );
   }
 }
-
